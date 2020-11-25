4b9b8afb3f678044af07e028df217ca39f746cc5
Daniel Ponce de León <dannzero@gmail.com>
2020-11-25 15:44:59 -0600

    Initial Commit

---
+++ CGrafica/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CGrafica
+//
+//  Created by Daniel Ponce de León on 25/11/20.
+//  Copyright © 2020 Daniel Ponce de León. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
